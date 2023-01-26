72730f4c02c59cb996d68c1c32c36a0fe68dbc82
Wombbus <107441667+Wombbus@users.noreply.github.com>
2023-01-26 16:16:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,72 +1,27 @@
-x = 0;
-y = 0;
-screen_width = 0
-screen_height = 0
-apple = ""
-draw_apple = "";
-to_number = ""
+function preload(){
 
-
-var SpeechRecognition = window.webkitSpeechRecognition;
-
-var recognition = new SpeechRecognition();
-
-function start() {
-  document.getElementById("status").innerHTML = "System is listening please speak";
-  recognition.start();
 }
 
-recognition.onresult = function (event) {
-
-  console.log(event);
-
-  content = event.results[0][0].transcript;
-
-  document.getElementById("status").innerHTML = "The speech has been recognized: " + content;
-
-  to_number = Number(content)
-
-  if (Number.isInteger(to_number)) {
-    draw_apple = "set"
-  }
-  else {
-    document.getElementById("status").innerHTML = "The speech has not recognized a number"
-  }
+function setup(){
+canvas = createCanvas(600,500)
+canvas.center()
+video = createCapture(VIDEO)
+video.hide()
+}
+function draw(){
+image(video, 100,100, 400,300)
+fill("red")
+circle(50, 50, 60);
+circle(50,460,60)
+circle(550,50,60)
+circle(550,460,60)
+fill("green")
+rect(78,50,445,20)
+rect(40,80 ,20,350)
+rect(545,80,20,350)
+rect(78,450,445,20)
 }
 
-function setup() {
-  screen_width = window.innerWidth
-  screen_height = window.innerHeight
-
-  canvas = createCanvas(screen_width, screen_height - 150)
-
-  canvas.position(0 - 150)
-}
-
-function draw() {
-  if (draw_apple == "set") {
-    for (i = 1; i <= to_number; i += 1) {
-      x = Math.floor(Math.random * 700)
-      y = Math.floor(Math.random*400)
-      image(apple,x,y,50,50)
-    }
-    document.getElementById("status").innerHTML = to_number + " Apples drawn";
-    draw_apple = "";
-    speak_data = to_number + "Apples drawn"
-    speak()
-  }
-}
-
-function speak() {
-  var synth = window.speechSynthesis;
-
-  var utterThis = new SpeechSynthesisUtterance(speak_data);
-
-  synth.speak(utterThis);
-
-  speak_data = "";
-}
-
-function preload() {
-  apple = loadImage("apple.png")
+function take_snapshot(){
+    save("filter.png")
 }
